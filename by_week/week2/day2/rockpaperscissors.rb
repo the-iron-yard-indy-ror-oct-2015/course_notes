a2f6463ba29564874cc53a0e55bdc5699d8b9d45
Chris Vannoy <chris.vannoy@theironyard.com>
2015-10-13 10:11:15 -0400

    Homework review final

--- by_week/week2/day2/rockpaperscissors.rb
+++ by_week/week2/day2/rockpaperscissors.rb
@@ -4,13 +4,35 @@ class Game
 
   def play
     puts "Hi. Let's play Rock, Paper, Scissors. 1, 2, 3 ... SHOOT!"
-    self.computer = %w(rock paper scissors).sample
+    self.computer = shoot!
     player_pick
     if round == "tie"
       puts "Player: #{player_one_choice.upcase} vs Computer: #{computer.upcase} ----- TIE"
     else
       puts "Player: #{player_one_choice.upcase} vs Computer: #{computer.upcase} ----- #{round.upcase} WINS"
     end
+  end
+
+  def bulk_play(num)
+    results = {:rock => 0, :paper => 0, :scissors => 0, :ties => 0}
+    num.to_i.times do
+      self.computer = shoot!
+      self.player_one_choice = shoot!
+      if round == "computer"
+        results[computer.to_sym] = results[computer.to_sym] + 1
+      elsif round == "player"
+        results[player_one_choice.to_sym] += 1
+      else
+        results[:ties] += 1
+      end
+    end
+    results.each do |key, value|
+      puts "#{key.to_s.upcase} won #{value} times"
+    end
+  end
+
+  def shoot!
+    %w(rock paper scissors).sample
   end
 
   def player_pick
@@ -45,4 +67,8 @@ end
 
 
 game = Game.new
-game.play
+if ARGV[0] == "bulk"
+  game.bulk_play(ARGV[1] || 1000)
+else
+  game.play
+end
