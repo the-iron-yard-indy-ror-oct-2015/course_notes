a5ca7f8e8645c22ecc8772f5326d2eae1f878b39
Chris Vannoy <chris.vannoy@theironyard.com>
2015-12-03 00:05:22 -0500

    Pagination. Including DIY paginate view helper

--- by_week/week8/day4/indy_rubyists/app/controllers/coders_controller.rb
+++ by_week/week8/day4/indy_rubyists/app/controllers/coders_controller.rb
@@ -1,11 +1,12 @@
 class CodersController < ApplicationController
 
   def index
-    users = Octokit.search_users(
+    @all_users = Octokit.search_users(
               "location:Indianapolis language:Ruby",
               :sort => "joined",
-              :order => :asc)[:items]
-    @users = users.collect{|u| Octokit.user(u[:id])}
+              :order => :asc,
+              :page => params[:page])
+    @users = @all_users[:items].collect{|u| Octokit.user(u[:id])}
   end
 
 end
